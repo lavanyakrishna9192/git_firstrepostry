2edf1267c5ec3a42f2e6e720e982291e4f919845
lavanyakrishna9192 <lavanyakrishna9192@gmail.com>
2023-10-01 19:25:03 +0530

    Given task was completed

---
+++ src/com/git/GitProgram.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class GitProgram {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("hai git");
+		System.out.println("This is lavanya");
+	}
+	
+}
